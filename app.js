f9dce443270c946d48b938e6bc00290786426cca
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-02-06 18:53:36 -0500

    node-postgres

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
 const express = require("express");
 const morgan = require("morgan");
-const postBank = require("./postBank");
+// const postBank = require("./postBank");
 const postList = require("./views/postList");
 const postDetails = require("./views/postDetails");
+const client = require("./db/index.js");
+const res = require("express/lib/response");
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  const posts = postBank.list();
-  res.send(postList(posts));
+app.get("/", async(req, res, next) => {
+  try {
+    const data = await client.query('SELECT * FROM posts');
+
+    
+    const posts = data.rows;
+    
+    res.send(postList(posts));
+  }
+  catch(ex) {
+    next(ex);
+  }
 });
 
-app.get("/posts/:id", (req, res) => {
-  const post = postBank.find(req.params.id);
-  res.send(postDetails(post));
+app.get("/posts/:id", async(req, res, next) => {
+  // const post = postBank.find(req.params.id);
+  try {
+    const data = await client.query('SELECT * FROM posts WHERE id=$1', [req.params.id]);
+    const post = data.rows[0];
+
+    res.send(postDetails(post));
+
+  } catch(ex) {
+    next(ex);
+  }
 });
 
-const PORT = 1337;
+const setUp = async() => {
+  try {
+    // await client.connect();
+    console.log('connected to database');
+    const PORT = 1337;
+    app.listen(PORT, () => console.log(`App listening in port ${PORT}`))
+  } 
+  catch(ex) {
+    console.log(ex);
+  }
+}
+setUp();
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+// const PORT = 1337;
+
+// app.listen(PORT, () => {
+//   console.log(`App listening in port ${PORT}`);
+// });
